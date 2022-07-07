28c340a710953663ae84ad737bb088d566f445bc
songpengfei <281218023@qq.com>
2022-07-07 11:06:17 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rollup-plugin-banner-obj
 you can specify the banner&amp;footer to be set for the specified chunk 
+# example 
+    output:{
+        bannerObj: { 'test.es.js': `import "./style.css"` },
+        footerObj: { 'test.es.js': `console.log(version)` }
+    }
+
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+export default function () {
+    let bannerObj = {}
+    let footerObj = {}
+    return {
+      name: 'banner-obj', 
+      options: function (options) {
+        bannerObj = options.output.bannerObj
+        footerObj = options.output.footerObj
+      },
+      generateBundle: function (options, bundle, isWrite) {
+        if (Object.keys(bannerObj).length > 0) {
+          Object.keys(bannerObj).forEach(e => {
+            bundle[e] && (bundle[e].code = `${bannerObj[e]}\n${bundle[e].code}`)
+          })
+        }
+        
+      }
+    }
+  }
+  
