65ef0d77c4d5f4f1bdbb6744753e702a6ecd1fdd
songpengfei <281218023@qq.com>
2022-07-07 11:41:59 +0800

    feat: add footerObj

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 function Objbanner () {
-    let bannerObj = {}
-    let footerObj = {}
-    return {
-      name: 'banner-obj', 
-      options: function (options) {
-        bannerObj = options.output.bannerObj
-        footerObj = options.output.footerObj
-      },
-      generateBundle: function (options, bundle, isWrite) {
-        if (Object.keys(bannerObj).length > 0) {
-          Object.keys(bannerObj).forEach(e => {
-            bundle[e] && (bundle[e].code = `${bannerObj[e]}\n${bundle[e].code}`)
-          })
-        }
-        
+  let bannerObj = {}
+  let footerObj = {}
+  return {
+    name: 'banner-obj', 
+    options: function (options) {
+      bannerObj = options.output.bannerObj
+      footerObj = options.output.footerObj
+    },
+    generateBundle: function (options, bundle, isWrite) {
+      if (Object.keys(bannerObj).length > 0) {
+        Object.keys(bannerObj).forEach(e => {
+          bundle[e] && (bundle[e].code = `${bannerObj[e]}\n${bundle[e].code}`)
+        })
       }
+      if (Object.keys(footerObj).length > 0) {
+        Object.keys(footerObj).forEach(e => {
+          bundle[e] && (bundle[e].code = `${bundle[e].code}${footerObj[e]} \n`)
+        })
+      }
+      
     }
   }
-  exports.Objbanner = Objbanner;
+}
+exports.Objbanner = Objbanner;
